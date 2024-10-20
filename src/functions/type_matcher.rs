fe7818fb4bf1ae6ee071a0dd28d2742349cbdc60
AmrDeveloper <amr96@programmer.net>
2024-10-20 16:57:24 +0200

    feat: Support Float 32 and 64 type matchers

--- src/functions/type_matcher.rs
+++ src/functions/type_matcher.rs
@@ -6,18 +6,27 @@ use gitql_core::values::base::Value;
 
 use crate::ir::types::TypeMatcherType;
 use crate::ir::values::TypeMatcherValue;
+use crate::matchers::type_matcher::FloatTypeMatcher;
+use crate::matchers::type_matcher::FloatTypeSize;
 use crate::matchers::type_matcher::IntTypeMatcher;
 use crate::matchers::type_matcher::IntTypeSize;
 use crate::matchers::type_matcher::VoidTypeMatcher;
 
 #[inline(always)]
 pub fn register_type_matchers_functions(map: &mut HashMap<&'static str, Function>) {
+    // Matcher for Void type
     map.insert("m_void", match_void);
+
+    // Matchers for Integer types
     map.insert("m_int1", match_int1);
     map.insert("m_int8", match_int8);
     map.insert("m_int16", match_int16);
     map.insert("m_int32", match_int32);
     map.insert("m_int64", match_int64);
+
+    // Matchers for Float types
+    map.insert("m_f32", match_float32);
+    map.insert("m_f64", match_float64);
 }
 
 #[inline(always)]
@@ -59,6 +68,20 @@ pub fn register_type_matchers_function_signatures(map: &mut HashMap<&'static str
     );
     map.insert(
         "m_int64",
+        Signature {
+            parameters: vec![],
+            return_type: Box::new(TypeMatcherType),
+        },
+    );
+    map.insert(
+        "m_f32",
+        Signature {
+            parameters: vec![],
+            return_type: Box::new(TypeMatcherType),
+        },
+    );
+    map.insert(
+        "m_f64",
         Signature {
             parameters: vec![],
             return_type: Box::new(TypeMatcherType),
@@ -115,6 +138,26 @@ pub fn match_int32(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
 pub fn match_int64(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
     let int1_matcher = IntTypeMatcher {
         size: IntTypeSize::Size64,
+    };
+
+    Box::new(TypeMatcherValue {
+        matcher: Box::new(int1_matcher),
+    })
+}
+
+pub fn match_float32(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
+    let int1_matcher = FloatTypeMatcher {
+        size: FloatTypeSize::Size32,
+    };
+
+    Box::new(TypeMatcherValue {
+        matcher: Box::new(int1_matcher),
+    })
+}
+
+pub fn match_float64(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
+    let int1_matcher = FloatTypeMatcher {
+        size: FloatTypeSize::Size64,
     };
 
     Box::new(TypeMatcherValue {
