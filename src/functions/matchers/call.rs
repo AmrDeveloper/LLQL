2230e5fe5174e0ed28a35cbf1f08d49eb9038412
AmrDeveloper <amr96@programmer.net>
2025-08-02 14:42:40 +0200

    Implement `m_intrinsic` inst matcher

--- src/functions/matchers/call.rs
+++ src/functions/matchers/call.rs
@@ -7,18 +7,29 @@ use gitql_core::values::Value;
 use crate::ir::types::InstMatcherType;
 use crate::ir::values::InstMatcherValue;
 use crate::matchers::call::CallInstMatcher;
+use crate::matchers::call::IntrinsicInstMatcher;
 
 #[inline(always)]
 pub fn register_call_inst_matchers_functions(map: &mut HashMap<&'static str, StandardFunction>) {
     map.insert("m_call", match_call_inst);
+    map.insert("m_intrinsic", match_intrinsic_inst);
 }
 
 #[inline(always)]
 pub fn register_call_inst_matchers_function_signatures(map: &mut HashMap<&'static str, Signature>) {
     map.insert("m_call", Signature::with_return(Box::new(InstMatcherType)));
+    map.insert(
+        "m_intrinsic",
+        Signature::with_return(Box::new(InstMatcherType)),
+    );
 }
 
 fn match_call_inst(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
     let matcher = Box::new(CallInstMatcher::create_call());
+    Box::new(InstMatcherValue { matcher })
+}
+
+fn match_intrinsic_inst(_values: &[Box<dyn Value>]) -> Box<dyn Value> {
+    let matcher = Box::new(IntrinsicInstMatcher::create_call());
     Box::new(InstMatcherValue { matcher })
 }
--- src/matchers/call.rs
+++ src/matchers/call.rs
@@ -1,4 +1,7 @@
+use inkwell::llvm_sys::core::LLVMGetCalledValue;
 use inkwell::llvm_sys::core::LLVMGetInstructionOpcode;
+use inkwell::llvm_sys::core::LLVMGetIntrinsicID;
+use inkwell::llvm_sys::core::LLVMIsACallInst;
 use inkwell::llvm_sys::prelude::LLVMValueRef;
 use inkwell::llvm_sys::LLVMOpcode;
 
@@ -17,5 +20,28 @@ impl Matcher<LLVMValueRef> for CallInstMatcher {
     #[allow(clippy::not_unsafe_ptr_arg_deref)]
     fn is_match(&self, instruction: &LLVMValueRef) -> bool {
         unsafe { LLVMGetInstructionOpcode(*instruction) == LLVMOpcode::LLVMCall }
+    }
+}
+
+#[derive(Clone)]
+pub struct IntrinsicInstMatcher;
+
+impl IntrinsicInstMatcher {
+    pub fn create_call() -> Self {
+        IntrinsicInstMatcher
+    }
+}
+
+impl Matcher<LLVMValueRef> for IntrinsicInstMatcher {
+    #[allow(clippy::not_unsafe_ptr_arg_deref)]
+    fn is_match(&self, instruction: &LLVMValueRef) -> bool {
+        unsafe {
+            if !LLVMIsACallInst(*instruction).is_null() {
+                let called_value = LLVMGetCalledValue(*instruction);
+                let intrinsic_id = LLVMGetIntrinsicID(called_value);
+                return intrinsic_id != 0;
+            }
+            false
+        }
     }
 }
